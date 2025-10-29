926d91b0be9f5f98dfaf5e8d9277f061e418256f
Khan Usaid <mohdusaid7272@gmail.com>
2025-10-29 07:57:14 +0530

    Add files via upload

---
+++ Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoRazor.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = "";
+        public string? Notes { get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateTime? DueDate { get; set; }
+        public DateTime AddedOn { get; set; } = DateTime.Now;
+    }
+}
